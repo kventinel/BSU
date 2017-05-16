e8f68e0a6da91fdde8cb6e4075e08ed666537270
Rak Alexey <kventinel@gmail.com>
2017-05-16 17:15:40 +0300

    update UP/Laba10

---
+++ 2_course/2_sem/UP/Laba10/Part2/func.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function call(){
+alert("Thank you!");
+}
