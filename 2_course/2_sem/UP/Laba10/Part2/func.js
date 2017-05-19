ec9a5977a79598399af2ef1a37e96aa43d8dc889
Rak Alexey <kventinel@gmail.com>
2017-05-19 17:15:45 +0300

    add UP Laba 10

--- 2_course/2_sem/UP/Laba10/Part2/func.js
+++ 2_course/2_sem/UP/Laba10/Part2/func.js
@@ -1,11 +1,14 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
 }
-
 function call(){
 alert("Thank you!");
 }
